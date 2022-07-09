2df0c9753d3ef2ead72890250bcf252ae0782cb9
TheLastKing711 <lastking711@protonmail.com>
2022-07-10 00:56:34 +0300

    refactor env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  movie_site_url: "https://www.themoviedb.org/",
+  app_url: "https://api.themoviedb.org/3/",
+  images_url: "https://image.tmdb.org/t/p/original",
+  api_key: "199b97e5064409cefa0526cf895e78b6"
 };
